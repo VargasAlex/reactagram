715d9d0a045aedf7a4ddeb1e4524069a68ecbcdc
Alex Vargas <alexv2489@gmail.com>
2018-10-05 11:18:55 -0400

    add post and props

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ import Post from './Post';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <Header />
-        <div>
-          <Post />
-        </div>
-      </div>
-    );
+    return <div className="App">
+      <Header />
+      <section className="App-main">
+        <Post nickname="Chris" avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Moving the community!" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
+        <Post nickname="OG" avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Holding a mic" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
+
+        {/* more posts */}
+      </section>
+    </div>;
   }
 }
 
