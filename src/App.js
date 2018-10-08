6c62a6dea4224bb7efdd00145793003e24fb60d9
Alex Vargas <alexv2489@gmail.com>
2018-10-08 14:30:35 -0400

    install Apollo and connect to react

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import ApolloClient from 'apollo-boost';
+const client = new ApolloClient({
+  uri: "http://localhost:3000"
+})
+
 import Header from './Header';
 import Post from './Post';
+import { ApolloProvider } from 'react-apollo';
 
 class App extends Component {
   render() {
-    return <div className="App">
-      <Header />
-      <section className="App-main">
-        <Post nickname="Chris" avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Moving the community!" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
-        <Post nickname="OG" avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Holding a mic" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
-
-        {/* more posts */}
-      </section>
-    </div>;
-  }
-}
+    return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Header />
+        <section className="App-main">
+          <Post />
+        </section>
+      </div>;
+    </ApolloProvider >
+  );
+ };
+};
 
 export default App;
