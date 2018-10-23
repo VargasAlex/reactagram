e9f1a4530199eac1ecc81138bb2d8789881dc62c
Alex Vargas <alexv2489@gmail.com>
2018-10-23 15:12:17 -0400

    set up pusher client

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './Header';
 import Posts from './Posts';
 
+import Pusher from 'pusher-js';
 
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
@@ -13,16 +14,29 @@ const client = new ApolloClient({
 })
 
 
-const App = () => {
-  return (
-    <ApolloProvider client={client}>
-      <div className="App">
-        <Header />
-        <section className="App-main">
-          <Posts />
-        </section>
-      </div>
-    </ApolloProvider>
-  );
-};
+class App extends Component {
+  constructor() {
+    super();
+    // connect to pusher
+    this.pusher = new Pusher("PUSHER_APP_KEY", {
+      cluster: 'eu',
+      encrypted: true
+    });
+  }
+
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div className="App">
+          <Header />
+          <section className="App-main">
+            {/* pass the pusher object and apollo to the posts component */}
+            <Posts pusher={this.pusher} apollo_client={client} />
+          </section>
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
+
 export default App;
